ed99e9969850c386b930db97f23652676f7f3ef0
Martin Leung <martikl1@uci.edu>
2018-07-13 14:48:04 -0700

    Add files via upload
    Beginning - Able to lookup ticker information (Ex: $AAPL, $MSFT, etc.)

---
+++ API Stock Project.py
@@ -0,0 +1,33 @@
+import json
+import urllib.parse
+import urllib.request
+
+url_base = "https://api.iextrading.com/1.0/"
+
+def ticker_lookup():
+    while True:
+        try:
+            ticker = input("What symbol do you want to look up? (AAPL, MSFT, ETC.) or 'Q' to quit: ")
+            if ticker.upper() == "Q":
+                print("Quitting Ticker Look Up")
+                break
+            ticker_url = url_base +"stock/" + ticker + "/book"
+            #print(ticker_url)
+            response = urllib.request.urlopen(ticker_url)
+            data = json.loads(response.read())
+            print()
+            print("Ticker: " + data['quote']['symbol'])
+            print("Company Name: " + data['quote']['companyName'])
+            print("Latest Price: " + str(data['quote']['latestPrice'])+"\n")
+        except:
+            print("Invalid Ticker")
+
+
+
+##for i in (response.read()):
+##    print(i)
+
+if __name__ == "__main__":
+    ticker_lookup()
+    
+
