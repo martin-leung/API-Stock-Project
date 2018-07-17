764e615ea2281b9091e7ba99692913c4b3eb25da
Martin Leung <m.leung@proceptcorp.local>
2018-07-17 14:35:19 -0700

    Implement charts now

--- API Stock Project.py
+++ API Stock Project.py
@@ -5,16 +5,18 @@ import urllib.request
 url_base = "https://api.iextrading.com/1.0/"
 
 def ticker_lookup():
+    print("TICKER LOOK UP")
     while True:
         try:
-            ticker = input("What symbol do you want to look up? (AAPL, MSFT, ETC.) or 'Q' to quit: ")
+            ticker = input("What symbol do you want to look up? (AAPL, MSFT, etc.) or 'Q' to quit: ")
             if ticker.upper() == "Q":
-                print("Quitting Ticker Look Up")
+                print("\nQuitting Ticker Look Up")
                 break
             ticker_url = url_base +"stock/" + ticker + "/book"
-            #print(ticker_url)
+            print(ticker_url)
             response = urllib.request.urlopen(ticker_url)
             data = json.loads(response.read())
+            #print(data)
             print()
             print("Ticker: " + data['quote']['symbol'])
             print("Company Name: " + data['quote']['companyName'])
@@ -23,8 +25,57 @@ def ticker_lookup():
             print("Invalid Ticker")
 
 
+def ticker_news():
+    print("\nTICKER NEWS SECTION:")
+    while True:
+        try:
+            ticker = input("Ticker (AAPL, MSFT, etc.) News: ")
+            if ticker.upper() == "Q":
+                print("Quitting Ticker News")
+                break
+            ticker_url = url_base + "stock/" + ticker + "/news/last/3"
+            response = urllib.request.urlopen(ticker_url)
+            data = json.loads(response.read())
+            print("Latest Headlines\n")
+            #print(data)
+            for each in data:
+                print("Headline: " + '"' + each['headline'] + '"')
+                print("URL: " + each['url'] + "\n")
+        except:
+            print("Invalid Ticker")
+
+def ticker_chart():
+    print("\nTICKER CHARTS:")
+    while True:
+        try:
+            ticker = input("Ticker (AAPL, MSFT, etc.) Chart: ")
+            if ticker.upper() == "Q":
+                print("Quitting Ticker News")
+                break
+            ticker_url = url_base + "/stock/" + ticker + "/chart"
+            response = urllib.request.urlopen(ticker_url)
+            data = json.loads(response.read())
+            #print(ticker_url)
+            print()
+            j = 0
+            for i in data:
+                if j >= 15:
+                    print(data[j]['date'], end = "  |  ")
+                j += 1
+            print()
+            j = 0
+            for i in data:
+                if j >= 15:
+                    print("    " + str(data[j]['open']), end = "     ") 
+                j += 1
+            print()
+        except:
+            print("Invalid Ticker")
 
 if __name__ == "__main__":
+
     ticker_lookup()
+    ticker_news()
+    ticker_chart()
     
 
