9e4d581284ccff01092ff9353f8246316798a3cb
martin-leung <martikl1@uci.edu>
2018-07-28 13:25:47 -0700

    Added UI main menu and other UI options

--- API Stock Project.py
+++ API Stock Project.py
@@ -5,12 +5,13 @@ import urllib.request
 url_base = "https://api.iextrading.com/1.0/"
 
 def ticker_lookup():
-    print("TICKER LOOK UP")
+    print("\nTICKER LOOK UP")
     while True:
         try:
             ticker = input("What symbol do you want to look up? (AAPL, MSFT, etc.) or 'Q' to quit: ")
             if ticker.upper() == "Q":
                 print("\nQuitting Ticker Look Up")
+                ticker_menu()
                 break
             ticker_url = url_base +"stock/" + ticker + "/book"
             print(ticker_url)
@@ -29,9 +30,10 @@ def ticker_news():
     print("\nTICKER NEWS SECTION:")
     while True:
         try:
-            ticker = input("Ticker (AAPL, MSFT, etc.) News: ")
+            ticker = input("Ticker (AAPL, MSFT, etc.) News or 'Q' to quit: ")
             if ticker.upper() == "Q":
                 print("Quitting Ticker News")
+                ticker_menu()
                 break
             ticker_url = url_base + "stock/" + ticker + "/news/last/3"
             response = urllib.request.urlopen(ticker_url)
@@ -48,9 +50,10 @@ def ticker_chart():
     print("\nTICKER CHARTS:")
     while True:
         try:
-            ticker = input("Ticker (AAPL, MSFT, etc.) Chart: ")
+            ticker = input("Ticker (AAPL, MSFT, etc.) Chart or 'Q' to quit: ")
             if ticker.upper() == "Q":
-                print("Quitting Ticker News")
+                print("Quitting Ticker News\n")
+                ticker_menu()
                 break
             ticker_url = url_base + "/stock/" + ticker + "/chart"
             response = urllib.request.urlopen(ticker_url)
@@ -72,10 +75,50 @@ def ticker_chart():
         except:
             print("Invalid Ticker")
 
-if __name__ == "__main__":
+def ticker_calc():
+    print("\nTICKER CALCULATOR:")
+    while True:
+        try:
+            ticker = input("Ticker (AAPL, MSFT, etc.) Chart: ")
+            if ticker.upper() == "Q":
+                print("Quitting Calculator\n")
+                break
+        except:
+            print("Invalid Ticker")
 
-    ticker_lookup()
-    ticker_news()
-    ticker_chart()
+def ticker_menu():
+    print("\nTicker Information Menu\n")
+    command = input("Options:\n    'P' - Current Price\n    'N' - News\n    'C' - Charts\n    'Q' - Quit Lookup\nCommand: ")
+    if command.upper() == "P":
+        ticker_lookup()
+    elif command.upper() == "N":
+        ticker_news()
+    elif command.upper() == "C":
+        ticker_chart()
+    elif command.upper() == "Q":
+        main_menu()
+    else:
+        print("Ticker Menu ERROR")
+
+def portfolio():
+    print("\nPortfolio Manager (In-Progress) \n")
+    command = input("Options:\n    'V' - View Portoflio\n    'A' - Add Ticker\n    'D' - Delete Ticker\n    'Q' - Quit\nCommand: ")
     
+
+def main_menu():
+    print("\nMAIN MENU\n")
+    command =  input("Options: \n    'L' - Lookup Ticker\n    'P' - Portfolio\n    'Q' - Quit Program\nCommand: ")
+    if command.upper() == "L":
+        ticker_menu()
+    elif command.upper() == "P":
+        portfolio()
+    elif command.upper() == "Q":
+        print("\nClosing App")
+    else:
+        print("\nERROR: Invalid Command")
+        main_menu()
+
+if __name__ == "__main__":
+    print("Welcome to your Portfolio Manager")
+    main_menu()
 
